35b2217e58807df7a4f4ba5170716e13fedce3a7
Daniel Jour <musteresel@gmail.com>
2020-03-05 09:07:16 +0100

    Extend example to show use of different restarts

--- mvp.cc
+++ mvp.cc
@@ -28,26 +28,42 @@ int main() {
                              std::cout << "not printed" << std::endl;
                            });
   fn_for<some_condition> a([] (some_condition) {
-                             std::cout << "This handler tries to issue a warning"
+                             std::cout << "Some condition came up! "
+                                       << "Ignore?"
                                        << std::endl;
+                             char c;
+                             std::cin >> c;
                              for (auto r : get_restarts()) {
                                std::cout << "  Trying restart: " << (void*)r
                                          << std::endl;
                                restart_with<warn> * w =
                                  dynamic_cast<restart_with<warn> *>(r);
-                               if (w) {
-                                 std::cout << "    => Works, RESTART!"
+                               restart_with<ignore> * i =
+                                 dynamic_cast<restart_with<ignore> *>(r);
+                               if ((c != 'y') && w) {
+                                 std::cout << "    => RESTART with warn!"
                                            << std::endl;
                                  w->restart({});
+                               } else if ((c == 'y') && i) {
+                                 std::cout << "    => RESTART with ignore!"
+                                           << std::endl;
+                                 i->restart({});
                                }
                              }
+                             std::cout << "Hm .. that's bad" << std::endl;
+                             assert(false);
                            });
   fn_for<some_condition> b([] (auto) {
                              std::cout << "This handler does nothing"
                                        << std::endl;
                            });
-  with_restart<warn>([]() { foo(); },
-                     [](auto) {
-                       std::cout << "WARNING" << std::endl;
-                     });
+  with_restart<ignore>([]() {
+                         with_restart<warn>([]() {
+                                              foo();
+                                            },
+                           [](auto) {
+                             std::cout << "WARNING" << std::endl;
+                           });
+                       },
+    [](auto) { /* do nothing */ });
 }
